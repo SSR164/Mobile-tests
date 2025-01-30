0ab0c2bea3fb5c1d7db9687d7144953d59a92340
Serg Sh. <vampir6307@gmail.com>
2025-01-30 21:27:17 +0300

    Mobile-tests2

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,40 +1,37 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import pages.WikiPage;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
-import static io.appium.java_client.AppiumBy.*;
-import static io.qameta.allure.Allure.step;
 
 public class SearchTests extends TestBase {
+    WikiPage wikiPage =new WikiPage();
+    String appiumElement = "Appium";
+    String koreaElement = "Korea";
+    String text= "Region in East Asia";
 
     @Test
+    @DisplayName("Поиск {appiumElement}")
     @Tag("android")
     void successfulSearchTest() {
-        step("Type search", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
-        });
-        step("Verify content found", () ->
-                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
+        wikiPage.clicSearchWikipedia();
+        wikiPage.searchElement(appiumElement);
+        wikiPage.checkElement(appiumElement);
+
+
     }
 
 
     @Test
+    @DisplayName("Поиск {koreaElement}")
     @Tag("android")
     void searchKoreaTest() {
-        step("Type search", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Korea");
-        });
-        $(By.id("org.wikipedia.alpha:id/page_list_item_description")).shouldHave(exactText("Region in East Asia"));
+        wikiPage.clicSearchWikipedia();
+        wikiPage.searchElement(koreaElement);
+        wikiPage.checkText(text);
+
 
     }
 }
